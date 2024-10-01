d1fd41afa53d77f4f59f0b38ed7c33a4abda13b8
ZahraThiaw <fatimatathiaw6@gmail.com>
2024-10-01 11:05:09 +0000

    update

--- src/main/java/sn/odc/oumar/springproject/Web/Controller/Impl/BaseControllerImpl.java
+++ src/main/java/sn/odc/oumar/springproject/Web/Controller/Impl/BaseControllerImpl.java
@@ -1,4 +1,4 @@
-package sn.odc.oumar.springproject.Web.Controller;
+package sn.odc.oumar.springproject.Web.Controller.Impl;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
